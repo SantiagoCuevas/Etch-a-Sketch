0e62a2dce49739179d1b12ae2661943c4e4c6d53
Santiago Cuevas <santiagocuevas77@gmail.com>
2023-09-13 14:04:28 -0600

    Create createGrid function.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-console.log("test");
+function createGrid(gridSize = 16) {
+  for (let i = 0; i < 16; i++) {
+    const container = document.querySelector("#container");
+    const row = document.createElement("div");
+
+    row.classList.add("row");
+
+    for (let i = 0; i < 16; i++) {
+      const tile = document.createElement("div");
+
+      tile.classList.add("tile");
+      row.appendChild(tile);
+    }
+
+    container.appendChild(row);
+  }
+}
+createGrid();
